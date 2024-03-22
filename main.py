866866035c6a0367b85bea132911f38cef8eda86
King-Sum <73327759+King-Sum@users.noreply.github.com>
2024-03-22 20:46:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import cv2
+import cv2 
 import numpy as np
 
-frame = cv2.imread('low_bright.jpeg')
-
-vid = cv2.VideoCapture(0)
-
-#create edge image
 def process(frame):
     threshold1 = 50
     threshold2 = 100
@@ -17,10 +12,48 @@ def process(frame):
     edges = cv2.Canny(image=blur, threshold1=50, threshold2=100) 
     return edges
 
+def BrightnessContrast(brightness=0): 
+	brightness = cv2.getTrackbarPos('Brightness', 'Control Panel') 
+	contrast = cv2.getTrackbarPos('Contrast', 'Control Panel') 
+	effect = controller(img, brightness, contrast) 
+	cv2.imshow('Effect', effect)
+    # cv2.imshow('Edge', process(effect))
+
+def controller(img, brightness=255, contrast=127): 
+	
+	brightness = int((brightness - 0) * (255 - (-255)) / (510 - 0) + (-255))
+	contrast = int((contrast - 0) * (127 - (-127)) / (254 - 0) + (-127))
+
+	if brightness != 0: 
+		if brightness > 0: 
+			shadow = brightness 
+			max = 255
+		else: 
+			shadow = 0
+			max = 255 + brightness 
+
+		al_pha = (max - shadow) / 255
+		ga_mma = shadow 
+		cal = cv2.addWeighted(img, al_pha, img, 0, ga_mma) 
+	else: 
+		cal = img 
+
+	if contrast != 0: 
+		Alpha = float(131 * (contrast + 127)) / (127 * (131 - contrast)) 
+		Gamma = 127 * (1 - Alpha) 
+		cal = cv2.addWeighted(cal, Alpha, cal, 0, Gamma)  
+
+	return cal 
+
+vid = cv2.VideoCapture(0)
+
+cv2.namedWindow('Control Panel') 
+cv2.createTrackbar('Brightness', 'Control Panel', 255, 2 * 255, BrightnessContrast)
+cv2.createTrackbar('Contrast', 'Control Panel', 127, 2 * 127, BrightnessContrast)
+
 while (True):
-    result, frame = vid.read()
-    cv2.imshow('frame', frame)
-    cv2.imshow('Edge', process(frame))
+    result, img = vid.read()
+    BrightnessContrast(0)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
